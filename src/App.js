e9b1d66d17dc4c50437907213aaa53b2400abc1d
datamarathon <datamarathonhiring@gmail.com>
2025-09-16 21:37:12 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { menu } from "./menu";
+import "./App.css";
+
+const phoneNumber = "9985788997";
+const secNumber="7993294071"; // replace with your business number
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {/* Header */}
+      <header className="header">
+        <img src="./logo.png" alt="Crave-Corner"></img>
+<span className="location">Your Craving-Our Corner</span>
       </header>
+
+      {/* Category Bubbles Row (No Filter) */}
+      <div className="category-row">
+        {menu.map((cat, i) => (
+          <div key={i} className="category-bubble">
+            <img
+              src={cat.image}
+              alt={cat.category}
+              className="category-img"
+            />
+            <span>{cat.category}</span>
+          </div>
+        ))}
+      </div>
+
+      {/* Items Display in 2 Columns */}
+      <div className="menu-list">
+        {menu.map((cat, i) => (
+          <div key={i} className="category-section">
+            <h2>{cat.category}</h2>
+            <div className="item-grid">
+              {cat.items.map((item, j) => (
+                <div key={j} className="item-card">
+                  <img
+                    src={item.image}
+                    alt={item.name}
+                    className="item-img"
+                  />
+                  <div className="item-info">
+                    <span className="item-name">{item.name}</span>
+                    <span className="item-price">₹{item.price}</span>
+                  </div>
+                  <a href={`tel:${phoneNumber}`} className="order-btn">
+                    📞 Order Now
+                  </a>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+      {/* footer */}
+
+      <div className="footer">
+        <h1>Order Now</h1>
+        <h1>Call at: {secNumber}</h1>
+        <h2>Best Deals for You and Your Gang Too..</h2>
+      </div>
     </div>
   );
 }
---
+++ src/menu.js
@@ -0,0 +1,89 @@
+
+ export const menu = [
+  {
+    category: "Shawarma",
+    image:"https://tse4.mm.bing.net/th/id/OIP.eEdE_Tx6aEbqBBEgen_6FQHaE7?pid=Api&P=0&h=180",
+    items: [
+      { name: "Regular Shawarma", price: 90, image: "https://tse4.mm.bing.net/th/id/OIP.FqgRH9sSMrqtb8EI9x4Y9AHaHa?pid=Api&P=0&h=180" },
+      { name: "Mint Salad Shawarma", price: 100, image: "https://tse4.mm.bing.net/th/id/OIP.eEdE_Tx6aEbqBBEgen_6FQHaE7?pid=Api&P=0&h=180" },
+      { name: "Peri Peri Shawarma", price: 120, image: "https://tse2.mm.bing.net/th/id/OIP.W96-FuALVMZBzidG0oNSdgHaE8?pid=Api&P=0&h=180" },
+      { name: "Cheese Shawarma", price: 120, image: "https://tse1.mm.bing.net/th/id/OIP.bv5NaMIAESKhM6mrTYCuWQHaEK?pid=Api&P=0&h=180" },
+      { name: "Dry Fruit Shawarma", price: 130, image: "https://tse1.mm.bing.net/th/id/OIP.YY8wfLnK_ICu1JeFnz5LVgAAAA?pid=Api&P=0&h=180" },
+      { name: "Paneer Shawarma Classic", price: 100, image: "https://tse4.mm.bing.net/th/id/OIP.VwGzcY1lPcj0het0ifYr3QHaE8?pid=Api&P=0&h=180" },
+      { name: "Paneer Peri Peri Shawarma", price: 120, image: "https://tse1.mm.bing.net/th/id/OIP.NVOgQSwbpxHuPiMqz2SUxQHaEP?pid=Api&P=0&h=180" },
+      { name: "Veg Shawarma", price: 80, image: "https://tse1.mm.bing.net/th/id/OIP.OmZbaBNcmZw5Ck1wFOwwBgHaEK?pid=Api&P=0&h=180" },
+      { name: "Egg Shawarma", price: 90, image: "https://tse3.mm.bing.net/th/id/OIP.9G0xt6QRyV-2PEQHaUbFHAHaHa?pid=Api&P=0&h=180" },
+      { name: "Layer Shawarma", price: 120, image: "https://tse4.mm.bing.net/th/id/OIP.ETWaZrpkjtqRlbSFYqZgcQHaHa?pid=Api&P=0&h=180" }
+    ]
+  },
+  {
+    category: "Waffles",
+    image:"https://tse1.mm.bing.net/th/id/OIP.a76FpDa_8WapAJ55tZzMvAHaK5?pid=Api&P=0&h=180",
+    items: [
+      { name: "Milk Chocolate Waffle", price: 99, image: "https://tse3.mm.bing.net/th/id/OIP.VaW7tA5wQ-lSeNQbu078owHaFd?pid=Api&P=0&h=180" },
+      { name: "White Chocolate Waffle", price: 99, image: "https://tse2.mm.bing.net/th/id/OIP.8q01WND4zb6ksW9EwRLixgHaEj?pid=Api&P=0&h=180" },
+      { name: "Dark Chocolate Waffle", price: 99, image: "https://tse4.mm.bing.net/th/id/OIP.aVvNUSLL12v8N5mG4cjkegHaHa?pid=Api&P=0&h=180" },
+      { name: "Oreo Chocolate Waffle", price: 99, image: "https://tse1.mm.bing.net/th/id/OIP.eO0U46RZpjK4lxLaRsji4QHaE8?pid=Api&P=0&h=180" },
+      { name: "KitKat Chocolate Waffle", price: 99, image: "https://tse3.mm.bing.net/th/id/OIP.M9daMO-ZH-HVFGfWhRncJwHaEK?pid=Api&P=0&h=180" },
+      { name: "Triple Chocolate Waffle", price: 99, image: "https://tse4.mm.bing.net/th/id/OIP._cJzgwiNdQbZwy2XPkdAVQHaHa?pid=Api&P=0&h=180" },
+      { name: "Nutella Chocolate Waffle", price: 99, image: "https://tse3.mm.bing.net/th/id/OIP.DMg-AUDLAjDityvzzogzggHaGL?pid=Api&P=0&h=180" },
+      { name: "Nutty Nutella Chocolate Waffle", price: 129, image: "https://tse3.mm.bing.net/th/id/OIP.NkrlNe4ZTXyVA3h20cobJAHaHa?pid=Api&P=0&h=180" }
+    ]
+  },
+  {
+    category: "Snacks",
+    image:"https://tse2.mm.bing.net/th/id/OIP.1ILUBI81oCG4FvHFhAkqqgHaFi?pid=Api&P=0&h=180",
+    items: [
+      { name: "Salted French Fries", price: 59, image: "https://tse1.mm.bing.net/th/id/OIP.LFdG3dZaBRgZFudzEfJG_QHaFJ?pid=Api&P=0&h=180" },
+      { name: "Peri Peri French Fries", price: 69, image: "hhttps://tse3.mm.bing.net/th/id/OIP.IOzHXfBdip-9bNJO4OinWgHaHa?pid=Api&P=0&h=180" },
+      { name: "Veg Nuggets", price: 69, image: "https://tse3.mm.bing.net/th/id/OIP.CCW3pfAXuoSxJeISLOgfUgHaLH?pid=Api&P=0&h=180" },
+      { name: "Chicken Nuggets", price: 89, image: "https://tse3.mm.bing.net/th/id/OIP.ovh-2HeLRNnzyGPgcRB5CgHaEJ?pid=Api&P=0&h=180" },
+      { name: "Veg Momos", price: 69, image: "https://tse4.mm.bing.net/th/id/OIP.Kw6_RnXlZ7w6J0K3_f4PCwHaLH?pid=Api&P=0&h=180" },
+      { name: "Chicken Momos", price: 89, image: "https://tse3.mm.bing.net/th/id/OIP.cOY-AbnHKZzA0Kpb7wBT1wHaHa?pid=Api&P=0&h=180" },
+      { name: "Chicken Popcorn", price: 99, image: "https://tse1.mm.bing.net/th/id/OIP.e26NDdbZaGRqSuSNZjOjOwHaHa?pid=Api&P=0&h=180" }
+    ]
+  },
+  {
+    category: "Fruity Delights",
+     image:"https://tse3.mm.bing.net/th/id/OIP.uonY282AhCmna-wuZgQhEgHaE8?pid=Api&P=0&h=180",
+    items: [
+      { name: "Red Mango Milkshake", price: 99, image: "https://tse4.mm.bing.net/th/id/OIP.YzBW01k1bYT9e41k0XfBrwHaLG?pid=Api&P=0&h=180" },
+      { name: "Red Mango Thickshake", price: 149, image: "https://tse1.mm.bing.net/th/id/OIP.-3SWojqr2cxzoy5CAcYzFQHaEK?pid=Api&P=0&h=180" },
+      { name: "Real Banana Milkshake", price: 99, image: "https://tse1.mm.bing.net/th/id/OIP.4P1WCl1qOc7pun_TYTR4dQHaHa?pid=Api&P=0&h=180" },
+      { name: "Real Banana Thickshake", price: 149, image: "https://tse2.mm.bing.net/th/id/OIP.D50bapx8a6i8pCp3MMRBnQHaHa?pid=Api&P=0&h=180" },
+      { name: "Strawberry Milkshake", price: 99, image: "https://tse1.mm.bing.net/th/id/OIP.a_H9zxwQ9GxrCgNc6J9jlgHaHa?pid=Api&P=0&h=180" },
+      { name: "Strawberry Thickshake", price: 149, image: "hhttps://tse2.mm.bing.net/th/id/OIP.tgx1ZDUHh41XYwWwwX-idwHaLH?pid=Api&P=0&h=180" },
+      { name: "Strawberry Choco Chips Milkshake", price: 99, image: "https://tse4.mm.bing.net/th/id/OIP.s21xfI26GcaO6At4tJBYOwHaKZ?pid=Api&P=0&h=180" },
+      { name: "Strawberry Choco Chips Thickshake", price: 149, image: "https://tse4.mm.bing.net/th/id/OIP.Tv_qZ8927-n1mESNfj9TuwHaLH?pid=Api&P=0&h=180" }
+    ]
+  },
+  {
+    category: "Nutty Delights",   image:"https://tse2.mm.bing.net/th/id/OIP.yLvpFtbUQqcJApw_IGMu5gHaLH?pid=Api&P=0&h=180",
+    items: [
+      { name: "Almond Mocha", price: 160, image: "https://tse1.mm.bing.net/th/id/OIP.ssM53DwhMFHZJlSv_XM6ugHaLG?pid=Api&P=0&h=180" }
+    ]
+  },
+  {
+    category: "Sandwiches & Burgers",
+    image:"https://tse1.mm.bing.net/th/id/OIP.OuaoiMa79RwKFM9HajdagwHaFP?pid=Api&P=0&h=180",
+    items: [
+      { name: "Chicken Sandwich", price: 99, image: "https://tse2.mm.bing.net/th/id/OIP.s2aTgAse5bkTiCCjZLgEAAHaE8?pid=Api&P=0&h=180" },
+      { name: "Veg Sandwich", price: 79, image: "https://tse3.mm.bing.net/th/id/OIP.jObSms1poB8lScX84OQ5wAHaLH?pid=Api&P=0&h=180" },
+      { name: "Paneer Sandwich", price: 89, image: "https://tse2.mm.bing.net/th/id/OIP.XHMQ19eCdFH-sQRUGhckNQHaFj?pid=Api&P=0&h=180" },
+      { name: "Chicken Burger", price: 99, image: "https://tse1.mm.bing.net/th/id/OIP.Y2mMyXzFxP4dHOXd3KrHBgHaE8?pid=Api&P=0&h=180" },
+      { name: "Veg Burger", price: 79, image: "https://tse2.mm.bing.net/th/id/OIP.xKmInOGnUTlE6bIJxmP8TgHaJ4?pid=Api&P=0&h=180" },
+      { name: "Paneer Burger", price: 89, image: "https://tse2.mm.bing.net/th/id/OIP.HQbTJ-IM0hvyhQahNfwr0QHaHa?pid=Api&P=0&h=180" }
+    ]
+  },
+  {
+    category: "Slushes",
+      image:"https://tse2.mm.bing.net/th/id/OIP.uGcR2ewN4u0gq92nuPykjgHaE8?pid=Api&P=0&h=180",
+    items: [
+      { name: "Mojito Mint", price: 69, image: "https://tse3.mm.bing.net/th/id/OIP.77HqeqPxlaEQuSxKt7NyXwHaHa?pid=Api&P=0&h=180" },
+      { name: "Blue Angel", price: 69, image: "https://tse3.mm.bing.net/th/id/OIP.3t8YKqSB0RRV1VYzyydRiQHaLH?pid=Api&P=0&h=180" },
+      { name: "Blue Berry", price: 69, image: "https://tse4.mm.bing.net/th/id/OIP.AnR--5wXLz5010ZZo3PncQHaJQ?pid=Api&P=0&h=180" },
+      { name: "Strawberry", price: 69, image: "https://tse3.mm.bing.net/th/id/OIP._7x4Mu8TZhgMg1iih0R9_QHaE7?pid=Api&P=0&h=180" },
+      { name: "Tangy Orange", price: 69, image: "https://tse1.mm.bing.net/th/id/OIP.EmhBFQjxiJgLnfJkQlwKUAHaJ0?pid=Api&P=0&h=180" }
+    ]
+  }
+];
